f1c23a71d5065ccfce913e1a545cb837afbb15ef
Ajay Kumar <kumarjangraajay522@gmail.com>
2026-05-05 19:27:02 +0530

    Fix submodule issue and add all React files

--- sapling-seeds/vite.config.js
+++ sapling-seeds/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/dist/',
   build: {
     chunkSizeWarningLimit: 600,
     target: 'esnext',
